de210e192ad2ccb8e4acdaa1bc3c04a4a35abce9
EC2 Default User <ec2-user@ip-172-31-16-199.ec2.internal>
2022-05-21 14:03:19 +0000

    commit7

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -16,11 +16,17 @@ class Admin::GenresController < ApplicationController
 
   def update
     @genre = Genre.find(params[:id])
-    redirect_to '/admin/genres'
+    if @genre.update(genre_params)
+      flash[:notice] = "変更内容を更新しました。"
+      redirect_to admin_genres_path
+    else
+      flash[:alert] = "変更内容に不備があります。"
+      render "edit"
+    end
   end
   
   private
-  # ストロングパラメータ
+  
   def genre_params
     params.require(:genre).permit(:name)
   end
